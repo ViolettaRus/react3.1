60aafc447bd8d42ad47f1718873bfbf158fbe974
Violetta Rusovich <violettarusovich@MacBook-Pro-Violetta.local>
2025-01-15 11:30:37 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './module/GameContainer';
+import App from './components/GameContainer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
